826c8cc7d839da46420e37ad22379a34ec0b9965
Ryan Baxter <rbaxter@vmware.com>
2020-09-14 10:54:53 -0400

    Add new header to response

--- name-service/src/main/java/com/example/demo/NameServiceApplication.java
+++ name-service/src/main/java/com/example/demo/NameServiceApplication.java
@@ -2,14 +2,21 @@ package com.example.demo;
 
 import java.util.Random;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class NameServiceApplication {
+
+	@Autowired
+	private Environment env;
 
 	public static final String[] NAMES = new String[]{"Paul", "John", "Ringo", "George"};
 	private static final Random r = new Random();
@@ -19,7 +26,8 @@ public class NameServiceApplication {
 	}
 
 	@GetMapping
-	public String getName() {
+	public String getName(HttpServletResponse response) {
+		response.addHeader("k8s-host", env.getProperty("HOSTNAME"));
 		return NAMES[r.ints(0, NAMES.length).limit(1).findFirst().getAsInt()];
 	}
 
